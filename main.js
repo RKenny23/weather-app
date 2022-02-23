05fc2e2fcc3401418f0e491396f8f8886272773b
RKenny23 <rkenny238@hotmail.com>
2022-02-23 17:01:34 -0500

    basic weather content displayed.

--- main.js
+++ main.js
@@ -1,24 +1,77 @@
 const userInput = document.getElementById("user-input");
 const searchBtn = document.getElementById("search-btn");
-const img = document.querySelector('img');
+const img = document.querySelector("img");
+const weatherData = document.getElementById("data");
+const cityName = document.getElementById("city-name");
+const weatherCode = document.getElementById("weather-code");
+const temp = document.getElementById("temp");
 
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  getWeather(userInput);
+  getWeather(userInput.value);
+  userInput.value = "";
 });
 
-async function getWeather(input) {
+async function getWeather(city) {
+  const apiURL = `http://api.weatherstack.com/current?access_key=0a1aafb568b3664b199d677b56cd4745&query=${city}`;
   try {
-    const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=p8dsvStRqgTtnAVT9fMfSvTsVTQJh8OF&s=${input.value}`,
-    { mode: "cors" }
-    );
-    const gifData = await response.json();
-    img.src = gifData.data.images.original.url;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    cityName.innerText = `Current weather for ${data.location.name}, ${data.location.region}`;
+
+    weatherCode.innerText = data.current.weather_descriptions[0];
+
+    const tempInF = Math.round(convertToF(data.current.temperature));
+    
+    temp.textContent = `${tempInF}\u00B0`
+
+    switch (data.current.weather_code) {
+      case 113:
+        img.src = "sunny.png";
+        break;
+      case 116:
+        img.src = "partly_cloudy.png";
+        break;
+      case 119:
+      case 122:
+      case 143:
+        img.src = "cloudy.png";
+        break;
+      case 296:
+      case 299:
+      case 302:
+      case 305:
+      case 308:
+        img.src = "heavy_rain.png";
+        break;
+      case 227:
+      case 230:
+      case 179:
+      case 368:
+      case 362:
+      case 332:
+      case 335:
+      case 326:
+        img.src = "snowy.png";
+        break;
+      case 200:
+        img.src = "thunderstorm.png";
+        break;
+    }
+    console.log(data)
 
   } catch (error) {
-    console.log("error");
-    img.src =
-      "https://upload.wikimedia.org/wikipedia/commons/3/34/ErrorMessage.png";
+    console.error(error);
+    cityName.innerText = "Please enter a valid city!";
+    weatherCode.innerText = "";
+    temp.textContent = "";
+    img.src = "";
+    // img.src =
+    //   "https://upload.wikimedia.org/wikipedia/commons/3/34/ErrorMessage.png";
   }
+}
+
+function convertToF(celsius) {
+  return celsius * (9 / 5) + 32;
 }
